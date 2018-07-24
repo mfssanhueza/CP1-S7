19827fc8f12a4f520516e2f837102641a38678fb
mfssanhueza <mfssanhueza@gmail.com>
2018-07-24 19:53:37 -0400

    Ejercicio5

---
+++ ejercicio5.rb
@@ -0,0 +1,5 @@
+meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo']
+ventas = [2000, 3000, 1000, 5000, 4000]
+ventas_mensuales = Hash[meses.zip ventas]
+invertido = ventas_mensuales.invert
+puts invertido.keys.max
