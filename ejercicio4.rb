9bc093c8a6754287c97564133b21393feb6b0da5
mfssanhueza <mfssanhueza@gmail.com>
2018-07-24 19:38:05 -0400

    ejercicio4

--- ejercicio4.rb
+++ ejercicio4.rb
@@ -1,2 +1,10 @@
-h = { claveuno: 10, clavedos: 20, clavetres: 30 }
-puts h
+personas = ["Carolina", "Alejandro", "Maria Jesús", "Valentín"]
+edades = [32, 28, 41, 19]
+edades_personas = Hash[personas.zip edades]
+
+def mostrar_edad (hash)
+  hash.each do |key, value| puts value
+  end
+end
+
+mostrar_edad(edades_personas)
